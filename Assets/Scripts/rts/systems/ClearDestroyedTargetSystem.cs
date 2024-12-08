6d8667dd181c864c389971d902516bc6ee6327f0
Dennis Federico <dennisfederico@gmail.com>
2024-12-08 14:49:44 +0100

    Refactor target reset sytem to use jobs

--- Assets/Scripts/rts/systems/ClearDestroyedTargetSystem.cs
+++ Assets/Scripts/rts/systems/ClearDestroyedTargetSystem.cs
@@ -1,5 +1,6 @@
 using rts.components;
 using Unity.Burst;
+using Unity.Collections;
 using Unity.Entities;
 using Unity.Transforms;
 
@@ -12,23 +13,66 @@ namespace rts.systems {
     [UpdateAfter(typeof(EndSimulationEntityCommandBufferSystem))]
     public partial struct ClearDestroyedTargetSystem : ISystem {
         
+        private ComponentLookup<LocalTransform> localTransformsLookup;
+        private EntityStorageInfoLookup entityStorageInfoLookup;
+        
         [BurstCompile]
         public void OnCreate(ref SystemState state) {
-            
+            localTransformsLookup = state.GetComponentLookup<LocalTransform>();
+            entityStorageInfoLookup = state.GetEntityStorageInfoLookup();
         }
 
         [BurstCompile]
         public void OnUpdate(ref SystemState state) {
-            foreach (var targetOverride in SystemAPI.Query<RefRW<TargetOverride>>()) {
-                if (targetOverride.ValueRO.Value == Entity.Null || !SystemAPI.Exists(targetOverride.ValueRO.Value) || !SystemAPI.HasComponent<LocalTransform>(targetOverride.ValueRO.Value)) {
-                    targetOverride.ValueRW.Value = Entity.Null;
-                }
-            }
             
-            foreach (var target in SystemAPI.Query<RefRW<Target>>()) {
-                if (target.ValueRO.Value == Entity.Null || !SystemAPI.Exists(target.ValueRO.Value) || !SystemAPI.HasComponent<LocalTransform>(target.ValueRO.Value)) {
-                    target.ValueRW.Value = Entity.Null;
-                }
+            localTransformsLookup.Update(ref state);
+            entityStorageInfoLookup.Update(ref state);
+
+            new ResetDestroyedTargetJob {
+                LocalTransformsLookup = localTransformsLookup,
+                EntityLookup = entityStorageInfoLookup
+            }.ScheduleParallel();
+            
+            // MOVED TO A JOB
+            // foreach (var target in SystemAPI.Query<RefRW<Target>>()) {
+            //     if (target.ValueRO.Value == Entity.Null || !SystemAPI.Exists(target.ValueRO.Value) || !SystemAPI.HasComponent<LocalTransform>(target.ValueRO.Value)) {
+            //         target.ValueRW.Value = Entity.Null;
+            //     }
+            // }
+            
+            new ResetDestroyedTargetOverrideJob {
+                LocalTransformsLookup = localTransformsLookup,
+                EntityLookup = entityStorageInfoLookup
+            }.ScheduleParallel();
+            
+            // MOVED TO A JOB
+            // foreach (var targetOverride in SystemAPI.Query<RefRW<TargetOverride>>()) {
+            //     if (targetOverride.ValueRO.Value == Entity.Null || !SystemAPI.Exists(targetOverride.ValueRO.Value) || !SystemAPI.HasComponent<LocalTransform>(targetOverride.ValueRO.Value)) {
+            //         targetOverride.ValueRW.Value = Entity.Null;
+            //     }
+            // }
+        }
+    }
+    
+    [BurstCompile]
+    public partial struct ResetDestroyedTargetJob : IJobEntity {
+
+        [ReadOnly] public ComponentLookup<LocalTransform> LocalTransformsLookup;
+        [ReadOnly] public EntityStorageInfoLookup EntityLookup;
+        public void Execute(ref Target target) {
+            if (!EntityLookup.Exists(target.Value) || !LocalTransformsLookup.HasComponent(target.Value)) {
+                target.Value = Entity.Null;
+            }
+        }
+    }
+    
+    public partial struct ResetDestroyedTargetOverrideJob : IJobEntity {
+
+        [ReadOnly] public ComponentLookup<LocalTransform> LocalTransformsLookup;
+        [ReadOnly] public EntityStorageInfoLookup EntityLookup;
+        public void Execute(ref TargetOverride targetOverride) {
+            if (!EntityLookup.Exists(targetOverride.Value) || !LocalTransformsLookup.HasComponent(targetOverride.Value)) {
+                targetOverride.Value = Entity.Null;
             }
         }
     }
